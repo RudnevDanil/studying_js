67f3c69bdfefdf61d397bfda14094f9e38c5fc04
Rudnev Danil <danil.160398@yandex.ru>
2021-01-18 15:03:41 +0400

    работа со staff

---
+++ 2 looters map preview/js/actionFunctions.js
@@ -0,0 +1,171 @@
+/*---------------------------- MENU --------------------------------------------*/
+let ids = {
+    screen: {
+        map: "mapScreen",
+        cameras: "camerasScreen",
+        cameraSettings: "cameraSettingsScreen",
+        aboutProject: "aboutProjectScreen",
+        faces: "facesScreen",
+        staff: "staffScreen",
+    },
+    menu: {
+        map: "menuMap",
+        cameras: "menuCameras",
+        cameraSettings: "menuCameraSettings",
+        aboutProject: "menuAboutProject",
+        faces: "menuFaces",
+        staff: "menuStaff",
+    },
+}
+
+function initZIndexForDisplay()
+{
+    document.getElementById(ids.screen.map).style.zIndex = "1";
+    document.getElementById(ids.screen.cameras).style.zIndex = "1";
+    document.getElementById(ids.screen.cameraSettings).style.zIndex = "1";
+    document.getElementById(ids.screen.aboutProject).style.zIndex = "1";
+    document.getElementById(ids.screen.faces).style.zIndex = "1";
+    document.getElementById(ids.screen.staff).style.zIndex = "1";
+}
+
+function initializeFontColorForDisplay()
+{
+    document.getElementById(ids.menu.map).style.color = "honeydew";
+    document.getElementById(ids.menu.cameras).style.color = "honeydew";
+    document.getElementById(ids.menu.cameraSettings).style.color = "honeydew";
+    document.getElementById(ids.menu.aboutProject).style.color = "honeydew";
+    document.getElementById(ids.menu.faces).style.color = "honeydew";
+    document.getElementById(ids.menu.staff).style.color = "honeydew";
+}
+
+function menuClicked(i)
+{
+    let screen = "", menu = "";
+    switch(i){
+        case 1:
+            screen = ids.screen.map;
+            menu = ids.menu.map;
+            break;
+        case 2:
+            screen = ids.screen.cameras;
+            menu = ids.menu.cameras;
+            break;
+        case 3:
+            screen = ids.screen.cameraSettings;
+            menu = ids.menu.cameraSettings;
+            break;
+        case 4:
+            screen = ids.screen.aboutProject;
+            menu = ids.menu.aboutProject;
+            break;
+        case 5:
+            screen = ids.screen.faces;
+            menu = ids.menu.faces;
+            break;
+        case 6:
+            screen = ids.screen.staff;
+            menu = ids.menu.staff;
+            break;
+    }
+    initZIndexForDisplay()
+    document.getElementById(screen).style.zIndex = "2";
+    initializeFontColorForDisplay()
+    document.getElementById(menu).style.color = "black";
+}
+
+/*---------------------------- MAP --------------------------------------------*/
+
+/*---------------------------- CAMERA --------------------------------------------*/
+function initCameraBlocks()
+{
+    for(let i = 1; i <= 4; i++)
+    {
+        document.getElementById("cameraBlock_" + i).style.w = "0";
+        document.getElementById("cameraBlock_" + i).className = "cameraBlock";
+        document.getElementById("placeholder_" + i).className = "";
+    }
+}
+
+function switchDisplayAmountCamerasTo_1()
+{
+    initCameraBlocks();
+    document.getElementById("cameraBlock_1").className += " ";
+    document.getElementById("cameraBlock_1").className += "amountCameras_1";
+    document.getElementById("placeholder_1").className = "testInside_1";
+}
+
+function switchDisplayAmountCamerasTo_2()
+{
+    initCameraBlocks();
+    for(let i = 1; i <= 4; i+=2)
+    {
+        document.getElementById("cameraBlock_" + i).className += " ";
+        document.getElementById("cameraBlock_" + i).className += "amountCameras_2";
+        document.getElementById("placeholder_" + i).className = "testInside_2_4";
+    }
+}
+
+function switchDisplayAmountCamerasTo_4()
+{
+    initCameraBlocks();
+    for(let i = 1; i <= 4; i++)
+    {
+        document.getElementById("cameraBlock_" + i).className += " ";
+        document.getElementById("cameraBlock_" + i).className += "amountCameras_4";
+        document.getElementById("placeholder_" + i).className = "testInside_2_4";
+    }
+}
+
+/*---------------------------- CAMERA SETTINGS --------------------------------------------*/
+
+function switchDisplayToUniSet()
+{
+    document.getElementById("streamSet").style.zIndex = "0";
+    document.getElementById("uniSet").style.zIndex = "2";
+}
+
+function switchDisplayToStreamSet()
+{
+    document.getElementById("uniSet").style.zIndex = "0";
+    document.getElementById("streamSet").style.zIndex = "2";
+}
+
+function switchDisplaySettingsToNextPage()
+{
+    document.getElementById("streamSetFirstPage").style.zIndex = "0";
+    document.getElementById("streamSetSecondPage").style.zIndex = "2";
+}
+
+function switchDisplaySettingsToPrevPage()
+{
+    document.getElementById("streamSetSecondPage").style.zIndex = "0";
+    document.getElementById("streamSetFirstPage").style.zIndex = "2";
+}
+
+/*---------------------------- ABOUT PROJECT --------------------------------------------*/
+
+/*---------------------------- FACES --------------------------------------------*/
+
+/*---------------------------- STAFF --------------------------------------------*/
+
+let actions_staff = {
+    addShow()
+    {
+        document.getElementById("staffListPage").style.zIndex = "0";
+        document.getElementById("staffAddPage").style.zIndex = "2";
+    },
+
+    listShow()
+    {
+        document.getElementById("staffAddPage").style.zIndex = "0";
+        document.getElementById("staffListPage").style.zIndex = "2";
+    },
+
+    nextShow() {document.getElementById("staffListNextButt").style.zIndex = "2";},
+    nextHide() {document.getElementById("staffListNextButt").style.zIndex = "0";},
+    prevShow() {document.getElementById("staffListPrevButt").style.zIndex = "2";},
+    prevHide() {document.getElementById("staffListPrevButt").style.zIndex = "0";},
+
+}
+
+
