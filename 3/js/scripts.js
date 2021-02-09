df54b2cdfea717b48cb65591c556fdbec160d7b7
Rudnev Danil <danil.160398@yandex.ru>
2021-02-09 10:21:33 +0400

    слот машина готова

--- 3/js/scripts.js
+++ 3/js/scripts.js
@@ -2,27 +2,44 @@ let settings = {
     imgNames: ["./data/trophy.png", "./data/coins.png", "./data/brilliant.png", "./data/lemon.png", "./data/banana.png", "./data/cherry.png"],
     multiplier: [-1, 100, 10, 5, 3, -2], // -1 is jackpot, -2 is a money back
     boxLineW: 4,
-    maxSteps: 8,
+    addMaxSteps: 10,
+    minSteps: 10,
+    speed: 5,
     oneElementMovingTime: 500, // ms
+
+    startJackpot: 7777,
+    startPlayerCoins: 100,
+    startBet: 10,
 }
 
 let state = {
-    jackpotValue: 277, // debug. should be 0
-    playerCoins: 45, // debug. should be 0
-    bet: 15, // debug. should be 0
-    inBoxNow: [
+    jackpotValue: 1277, // debug. should be 0
+    playerCoins: 145, // debug. should be 0
+    bet: 115, // debug. should be 0
+    inBoxNow: [ // store current images
         [-1,-1,-1,-1],
         [-1,-1,-1,-1],
         [-1,-1,-1,-1],
     ],
-    position: [0, 0, 0],
-    steps: [0, 0, 0],
-    stepsLeft: [0, 0, 0],
-    stepsInStart: [0, 0, 0],
-    lastStep: [0, 0, 0],
-    isImagesLoaded: false,
-    counterLoadedImages: 0,
-    animDuration: 2000,
+    position: [0, 0, 0], // position in animation
+    steps: [0, 0, 0], // counter in animation
+    stepsLeft: [0, 0, 0], // counter in animation
+    stepsInStart: [0, 0, 0], // counter in animation
+    lastStep: [0, 0, 0], // counter in animation
+    isImagesLoaded: false, // before this became true nothing will be painted
+    counterLoadedImages: 0, //
+    animDuration: 2000, // changes in depend of steps
+
+    centralFrameColor: "#e2bd4a",
+    playerCoinsColor: "white",
+    jackpotColor: "white",
+}
+
+let ids = {
+    go: "goBackground",
+    jackpotValue: "jackpotValueLine",
+    playerCoins: "creditValue",
+    bet: "betValue",
 }
 
 let images = [];
@@ -34,6 +51,14 @@ function init()
         document.getElementById("cnvs").width = document.getElementById("cnvs").getBoundingClientRect().width;
         document.getElementById("cnvs").height = document.getElementById("cnvs").getBoundingClientRect().height;
     }
+
+    $("#" + ids.jackpotValue).text(settings.startJackpot);
+    $("#" + ids.playerCoins).text(settings.startPlayerCoins);
+    $("#" + ids.bet).text(settings.startBet);
+
+    state.jackpotValue = settings.startJackpot;
+    state.playerCoins = settings.startPlayerCoins;
+    state.bet = settings.startBet;
 
     for(let i = 0; i < settings.imgNames.length; i++)
     {
@@ -61,8 +86,41 @@ function init()
     }
 }
 
-let ids = {
-    go: "goBackground",
+function betPlusClicked()
+{
+    let tryBetValue = parseInt($("#" + ids.bet).text());
+    let playerCoins = parseInt($("#" + ids.playerCoins).text());
+    if(tryBetValue + 1 > playerCoins)
+    {
+        // blink coins red / white
+        for(let i = 0; i < 2; i++)
+            setTimeout(function(){
+                $("#" + ids.playerCoins).css({'color': (i % 2 === 0 ? 'red': 'white')})
+                }, 500 * i);
+    }
+    else
+    {
+        $("#" + ids.bet).text(tryBetValue + 1);
+        state.bet = tryBetValue + 1;
+    }
+}
+
+function betMinusClicked()
+{
+    let tryBetValue = parseInt($("#" + ids.bet).text());
+    if(tryBetValue - 1 < 0)
+    {
+        // blink coins red / white
+        for(let i = 0; i < 2; i++)
+            setTimeout(function(){
+                $("#" + ids.bet).css({'color': (i % 2 === 0 ? 'red': 'white')})
+            }, 500 * i);
+    }
+    else
+    {
+        $("#" + ids.bet).text(tryBetValue - 1);
+        state.bet = tryBetValue - 1;
+    }
 }
 
 function goClicked()
@@ -70,13 +128,27 @@ function goClicked()
     if(state.isAnimationStarted)
         return;
 
-    // update  jackpotValue, playerCoins, bet
+    state.jackpotValue = parseInt($("#" + ids.jackpotValue).text());
+    state.playerCoins = parseInt($("#" + ids.playerCoins).text());
+    state.bet = parseInt($("#" + ids.bet).text());
+    $("#" + ids.playerCoins).text(state.playerCoins - state.bet);
+    state.playerCoins = parseInt($("#" + ids.playerCoins).text());
+
+    if(state.bet === 0)
+    {
+        // blink bet red / white
+        for (let i = 0; i < 2; i++)
+            setTimeout(function () {
+                $("#" + ids.bet).css({'color': (i % 2 === 0 ? 'red' : 'white')})
+            }, 500 * i);
+        return;
+    }
 
     state.isAnimationStarted = true;
     $("#" + ids.go).removeClass('goNotBordered').addClass('goBordered');
     for(let i = 0; i < 3; i++)
-        state.steps[i] = Math.floor(Math.random() * settings.maxSteps)
-    state.animDuration = settings.oneElementMovingTime * Math.max.apply(null, state.steps);
+        state.steps[i] = Math.floor(Math.random() * settings.addMaxSteps) + settings.minSteps
+    state.animDuration = settings.oneElementMovingTime * Math.max.apply(null, state.steps) / settings.speed;
     state.stepsLeft = [0, 0, 0];
     state.lastStep = [0, 0, 0];
     for(let i = 0; i < 3; i++)
@@ -92,7 +164,6 @@ function goClicked()
         {
             for(let i = 0; i < 3 ; i++)
             {
-                let i = 0;
                 let fractionsForI = 1 / state.stepsInStart[i];
                 if(state.steps[i] > 0)
                 {
@@ -116,15 +187,117 @@ function goClicked()
             requestAnimationFrame(animate);
         else
         {
-            state.isAnimationStarted = false;
-            $("#" + ids.go).removeClass('goBordered').addClass('goNotBordered');
-            // update  jackpotValue, playerCoins, bet
+            for(let i = 0; i < 3 ; i++)
+            {
+                if(state.position[i] > 0.5)
+                {
+                    console.log("+++")
+                    state.inBoxNow[i][3] = state.inBoxNow[i][2];
+                    state.inBoxNow[i][2] = state.inBoxNow[i][1];
+                    state.inBoxNow[i][1] = state.inBoxNow[i][0];
+                    state.inBoxNow[i][0] = findNextImg(i);
+
+                    state.position[i] = 0;
+                }
+            }
+            checkResults()
         }
     });
 
 }
 
-let w = null, h = null, boxOffset = null, box = null, strokeBox = null, strokeBoxes = null, boxes = null;
+/*  animationType description
+    0 - blink 3 times red / gold central frame
+    1 - blink 3 times green / gold central frame
+    2 - blink 3 times green / gold central frame and player coins green / white
+    3 - blink 5 times green / gold central frame and player coins green / white and green / white jackpot value
+ */
+function winAnimation(type)
+{
+    if(type === 1)
+    {
+        state.bet *= settings.multiplier[state.inBoxNow[0][2]];
+        $("#" + ids.bet).text(state.bet);
+    }
+
+    let baseFrameColor = state.centralFrameColor;
+    let addFrameColor = type === 0 ? "red" : "green";
+
+    let doBlinkJackpot = type === 3;
+    let doBlinkCoins = type === 2 || type === 3;
+    let doBlinkBet = type === 1 || type === 3;
+
+    let maxIteration = (type === 3 ? 12 : 6)
+    for(let i = 0; i < maxIteration; i++)
+        setTimeout(function(){
+            state.centralFrameColor = i % 2 === 0 ? addFrameColor : baseFrameColor;
+            reDrawCNVS();
+
+            if(doBlinkJackpot)
+                $("#" + ids.jackpotValue).css({'color': (i % 2 === 0 ? 'green': 'white')});
+
+            if(doBlinkCoins)
+                $("#" + ids.playerCoins).css({'color': (i % 2 === 0 ? 'green': 'white')});
+
+            if(doBlinkBet)
+                $("#" + ids.bet).css({'color': (i % 2 === 0 ? 'green': 'white')});
+
+
+        }, 500 * i);
+
+    setTimeout(function(){moneyOperation(type);},500 * (maxIteration));
+    setTimeout(function(){animationEnded();},500 * (maxIteration + 1));
+}
+
+function checkResults()
+{
+    let animationType = 0;
+    if(state.inBoxNow[0][2] === state.inBoxNow[1][2] && state.inBoxNow[0][2] === state.inBoxNow[2][2])
+    {
+        let multiplier = settings.multiplier[state.inBoxNow[0][2]]
+        if(multiplier == -1) // jackpot
+        {
+            animationType = 3;
+        }
+        else if(multiplier == -2) // money back
+        {
+            animationType = 2;
+        }
+        else
+        {
+            // it will be better count multiplier effect here, but it's happening in winAnimation(...) for more common tests
+            animationType = 1;
+        }
+    }
+    winAnimation(animationType)
+}
+
+function moneyOperation(type)
+{
+    if(type === 0 || type === 1)
+        state.jackpotValue += state.bet;
+    else if(type === 2)
+        state.playerCoins += state.bet;
+    else if(type === 3)
+    {
+        state.playerCoins += state.jackpotValue + state.bet;
+        state.jackpotValue = 0;
+    }
+
+    state.bet = state.playerCoins >= settings.startBet ? settings.startBet : state.playerCoins;
+    $("#" + ids.jackpotValue).text(state.jackpotValue);
+    $("#" + ids.playerCoins).text(state.playerCoins);
+    $("#" + ids.bet).text(state.bet);
+}
+
+function animationEnded()
+{
+    // call me when all spin actions are done.
+    state.isAnimationStarted = false;
+    $("#" + ids.go).removeClass('goBordered').addClass('goNotBordered');
+}
+
+let w = null, h = null, boxOffset = null, box = null, strokeBox = null, strokeBoxes = null, boxes = null, arrowOffsets = null;
 
 function countBoxSizes()
 {
@@ -137,7 +310,7 @@ function countBoxSizes()
     strokeBox = {w: w * 0.7 / 3, h: h};
     strokeBoxes = [{x: 2 * boxOffset.x, y: boxOffset.y}, {x: 3 * boxOffset.x + box.w, y: boxOffset.y}, {x: 4 * boxOffset.x + 2 * box.w, y: boxOffset.y}];
     boxes = [{x: 2 * boxOffset.x + lw/2, y: boxOffset.y + lw/2}, {x: 3 * boxOffset.x + box.w + lw/2, y: boxOffset.y + lw/2}, {x: 4 * boxOffset.x + 2 * box.w + lw/2, y: boxOffset.y + lw/2}];
-
+    arrowOffsets = {x: w / 20, y: h / 15}
 }
 
 function drawLines()
@@ -146,25 +319,24 @@ function drawLines()
     let ctx = cnvs.getContext('2d');
 
     // boxes
+    ctx.beginPath();
     ctx.lineWidth = 4;
     ctx.strokeStyle = "#e2bd4a";
-    //ctx.strokeRect(0,0,w,h)
     for(let i = 0; i < boxes.length; i++)
-    {
         ctx.strokeRect(strokeBoxes[i].x, strokeBoxes[i].y, strokeBox.w, strokeBox.h);
-
-    }
     ctx.stroke();
 
     // win box
+    ctx.beginPath();
     ctx.lineWidth = 7;
     ctx.strokeStyle = "black";
     ctx.strokeRect(boxOffset.x, h * 3 / 10, w - 2 * boxOffset.x, h * 4 / 10);
     ctx.lineWidth = 5;
-    ctx.strokeStyle = "#e2bd4a";
+    ctx.strokeStyle = state.centralFrameColor;//"#e2bd4a";
     ctx.strokeRect(boxOffset.x, h * 3 / 10, w - 2 * boxOffset.x, h * 4 / 10);
     ctx.stroke();
 
+    ctx.beginPath();
     ctx.fillStyle = "rgba(255,255,255,0.6)"
     for(let i = 0; i < boxes.length; i++)
     {
@@ -175,38 +347,38 @@ function drawLines()
 
     // left arrow
     ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.fillStyle = "#e2bd4a";
-    ctx.moveTo(0, h / 2 - boxOffset.y);
-    ctx.lineTo(boxOffset.x * 2 / 3, h / 2);
-    ctx.lineTo(0, h / 2 + boxOffset.y);
-    ctx.lineTo(0, h / 2 - boxOffset.y);
+    ctx.lineWidth = 2;
+    ctx.fillStyle = state.centralFrameColor;//"#e2bd4a";
+    ctx.moveTo(0, h / 2 - arrowOffsets.y);
+    ctx.lineTo(arrowOffsets.x * 2 / 3, h / 2);
+    ctx.lineTo(0, h / 2 + arrowOffsets.y);
+    ctx.lineTo(0, h / 2 - arrowOffsets.y);
     ctx.fill();
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.strokeStyle = "black";
-    ctx.moveTo(0, h / 2 - boxOffset.y);
-    ctx.lineTo(boxOffset.x * 2 / 3, h / 2);
-    ctx.lineTo(0, h / 2 + boxOffset.y);
-    ctx.lineTo(0, h / 2 - boxOffset.y);
+    ctx.moveTo(0, h / 2 - arrowOffsets.y);
+    ctx.lineTo(arrowOffsets.x * 2 / 3, h / 2);
+    ctx.lineTo(0, h / 2 + arrowOffsets.y);
+    ctx.lineTo(0, h / 2 - arrowOffsets.y);
     ctx.stroke();
 
     // right arrow
     ctx.beginPath();
     ctx.lineWidth = 5;
-    ctx.fillStyle = "#e2bd4a";
-    ctx.moveTo(w, h / 2 - boxOffset.y);
-    ctx.lineTo(w - boxOffset.x * 2 / 3, h / 2);
-    ctx.lineTo(w, h / 2 + boxOffset.y);
-    ctx.lineTo(w, h / 2 - boxOffset.y);
+    ctx.fillStyle = state.centralFrameColor;//"#e2bd4a";
+    ctx.moveTo(w, h / 2 - arrowOffsets.y);
+    ctx.lineTo(w - arrowOffsets.x * 2 / 3, h / 2);
+    ctx.lineTo(w, h / 2 + arrowOffsets.y);
+    ctx.lineTo(w, h / 2 - arrowOffsets.y);
     ctx.fill();
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.strokeStyle = "black";
-    ctx.moveTo(w, h / 2 - boxOffset.y);
-    ctx.lineTo(w - boxOffset.x * 2 / 3, h / 2);
-    ctx.lineTo(w, h / 2 + boxOffset.y);
-    ctx.lineTo(w, h / 2 - boxOffset.y);
+    ctx.moveTo(w, h / 2 - arrowOffsets.y);
+    ctx.lineTo(w - arrowOffsets.x * 2 / 3, h / 2);
+    ctx.lineTo(w, h / 2 + arrowOffsets.y);
+    ctx.lineTo(w, h / 2 - arrowOffsets.y);
     ctx.stroke();
 }
 
@@ -256,20 +428,4 @@ function reDrawCNVS()
     drawImages();
     drawLines();
 }
-
-/*animate({
-    duration: 200,
-    timing(timeFraction) {
-        return timeFraction;
-    },
-    drawClosing(progress, i) {
-        document.getElementById("card_"+i).style.width = 100 - progress * 100 + "%"; // close
-    },
-    i,
-});
-
-function animate({timing, drawClosing, duration,i})
-{
-
-}*/
 
