73effee864ff399e442ae4017f6fe7e099eeef55
Rudnev Danil <danil.160398@yandex.ru>
2021-02-19 09:00:03 +0400

    добавил файл бд

--- 2 looters map preview/php/test.php
+++ 2 looters map preview/php/test.php
@@ -1,15 +1,15 @@
 <?php
 echo "Привет мир!";
 
-$conn = mysqli_connect("localhost", "root", "111", "test");
+//$conn = mysqli_connect("localhost", "root", "111", "test");
 
-if ($conn == false) {echo "Ошибка: Невозможно подключиться к MySQL " , mysqli_connect_error();}
-else {echo "Соединение установлено успешно";}
+//if ($conn == false) {echo "Ошибка: Невозможно подключиться к MySQL " , mysqli_connect_error();}
+//else {echo "Соединение установлено успешно";}
 
 /*
 $sql = 'INSERT INTO cities SET name = "Санкт-Петербург"';
 $result = mysqli_query($link, $sql);
 if ($result == false) {echo "Произошла ошибка при выполнении запроса";}*/
 
-$conn->close();
+//$conn->close();
 ?>
