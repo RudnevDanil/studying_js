bd95c80d0c19d5d155e1d7fb9cd20f06feecb12d
Rudnev Danil <danil.160398@yandex.ru>
2021-03-10 10:09:14 +0300

    added only ids functionality

--- 2 looters map preview/php/loadTrainImgs.php
+++ 2 looters map preview/php/loadTrainImgs.php
@@ -6,20 +6,40 @@ $pass = $_GET['pass'];
 include "./checkAuth.php";
 include "./dbConnect.php";
 
-$query = "select img, staff_id from lm_faces where user_id='$userId' and staff_id IS NOT NULL;";
+$idsOnly = true;
+if(isset($_GET['idsOnly']) && !empty($_GET['idsOnly']) && $_GET['idsOnly'] == "true")
+    $query = "select id from lm_faces where user_id='$userId' and staff_id IS NOT NULL;";
+else
+{
+    $query = "select id, img, staff_id from lm_faces where user_id='$userId' and staff_id IS NOT NULL;";
+    $idsOnly = false;
+}
+//print_r("_".isset($_GET['idsOnly'])."_".!empty($_GET['idsOnly'])."_".($_GET['idsOnly'] == "true"));
 if ($result = $mysqli->query($query))
 {
     $arr = $result->fetch_all();// для всего массива
     $arr_f = [];
     $arr_n = [];
+    $arr_ids = [];
 
-    for ($i = 0; $i < count($arr); $i++)
+    if(!$idsOnly)
     {
-        array_push($arr_f, substr($arr[$i][0], 22));
-        array_push($arr_n, $arr[$i][1]);
+        for ($i = 0; $i < count($arr); $i++)
+        {
+            #array_push($arr_ids, $arr[$i][0]);
+            array_push($arr_f, substr($arr[$i][1], 22));
+            array_push($arr_n, $arr[$i][2]);
+        }
+        echo json_encode(array("answer" => "done", "arr" => $arr_f, "staff_id" => $arr_n/*, "ids" => $arr_ids*/));
     }
-
-    echo json_encode(array("answer" => "done", "arr" => $arr_f, "staff_id" => $arr_n));
+    else
+    {
+        for ($i = 0; $i < count($arr); $i++)
+        {
+            array_push($arr_ids, $arr[$i][0]);
+        }
+        echo json_encode(array("answer" => "done", "ids" => $arr_ids));
+    }
 }
 else
     echo json_encode(array("answer" => "query error"));
